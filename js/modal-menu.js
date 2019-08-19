97a431f1a36f94969e84d3809d0b2faadd360b97
Akest <gizat.blt@gmail.com>
2019-08-19 23:56:35 +0300

    Remove Scroll form modal-menu

--- js/modal-menu.js
+++ js/modal-menu.js
@@ -1,11 +1,14 @@
 let modal_open_button = document.querySelector('#modal-open-button');
 let modal_close_button = document.querySelector('#modal-close-button');
 let modal_menu = document.querySelector('#modal-menu');
+let body = document.querySelector('#body');
 
 modal_open_button.addEventListener('click', function(){
 	modal_menu.classList.add('modal-menu_active');
+	body.classList.add('stop-scroll');
 });
 
 modal_close_button.addEventListener('click', function(){
 	modal_menu.classList.remove('modal-menu_active');
+	body.classList.remove('stop-scroll');
 });
